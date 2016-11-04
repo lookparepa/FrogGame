3893a64ad706f79452e51d96e0a95260be8fa415
lookparepa <the.pa.re@hotmail.com>
2016-11-04 11:27:20 -0700

    add bg to asset

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -5,8 +5,10 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.FrogGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new FrogGame(), config);
-	}
+    public static void main (String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 900;
+        config.height = 500;
+        new LwjglApplication(new FrogGame(), config);
+    }
 }
