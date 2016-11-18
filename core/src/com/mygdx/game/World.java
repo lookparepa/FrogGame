f44a8ab1ffdc33c6c1f5ec0225ec9dca938faf4e
lookparepa <the.pa.re@hotmail.com>
2016-11-17 21:32:26 -0800

    car sound

--- core/src/com/mygdx/game/World.java
+++ core/src/com/mygdx/game/World.java
@@ -5,11 +5,16 @@ import java.util.List;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.math.Rectangle;
 
 public class World {
 	Frog frog;
 	FrogGame frogGame;
+	
+	Sound sound = Gdx.audio.newSound(Gdx.files.internal("car_sound.mp3"));
+	
+
 	int life = 10;
 	
 	List <Car> cars = new ArrayList <Car> ();
@@ -17,6 +22,7 @@ public class World {
 	World(FrogGame frogGame) {
 		this.frogGame = frogGame;
 		frog = new Frog();
+		sound.loop();
 		for (int i = 0 ; i < frogGame.TOTALCAR ; i++ ) {
 			cars.add(new Car(i));
 			car2s.add(new Car2(i));
